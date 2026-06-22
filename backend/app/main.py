7f189d790e5cf6a6089564e5df4247e7b2267d2f
Vinay Badnoriya <123866012+Vinay1727@users.noreply.github.com>
2026-06-22 18:40:15 +0530

    add media assets

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
+from fastapi.staticfiles import StaticFiles
 
 from app.core.config import settings
 from app.core.exceptions import AppException
@@ -44,6 +45,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.mount("/media", StaticFiles(directory="media"), name="media")
 
 
 @app.exception_handler(AppException)
